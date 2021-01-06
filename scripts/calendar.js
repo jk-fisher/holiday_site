c5451bbe133f2cf6b4f2e50372dab1573fcd4709
Joanna Fisher <fisherjoanna@sky.com>
2021-01-06 17:04:01 +0000

    renders calender to DOM

---
+++ scripts/calendar.js
@@ -0,0 +1,131 @@
+const date = new Date();
+
+const prevMonth = document.querySelector('.prev-month');
+const nextMonth = document.querySelector('.next-month');
+const prevYear = document.querySelector('.prev-year');
+const nextYear = document.querySelector('.next-year');
+const monthDays = document.querySelector('.days');
+
+const renderCalendar = () =>{
+    date.setDate(1);
+
+
+    const lastDate = new Date(
+        date.getFullYear(),
+        date.getMonth() + 1,
+        0
+        ).getDate();
+    
+    const prevLastDate = new Date(
+        date.getFullYear(),
+        date.getMonth(),
+        0
+        ).getDate();
+        console.log(prevLastDate);
+    
+    const firstDayIndex = date.getDay();
+    // console.log(firstDayIndex);
+    
+    
+    const lastDayIndex = new Date(
+        date.getFullYear(),
+        date.getMonth() + 1,
+        0
+        ).getDay();
+        console.log(lastDayIndex);
+
+    const nextMonthDays = 7 - lastDayIndex - 1;
+
+    const month = [ 
+        "January",
+        "February",
+        "March", 
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ];
+
+    document.querySelector('.month').innerHTML = month[date.getMonth()];
+    document.querySelector('.year').innerHTML = date.getFullYear();
+
+    document.querySelector('.selected-date').innerHTML = new Date().toDateString();
+
+    let days = "";
+
+    //count down to output last month days
+    for(x = firstDayIndex; x > 0; x--){
+        days += `<div class="prev-date day" id=${prevLastDate - x + 1}>${prevLastDate - x + 1}</div>`
+    };
+
+    //this month days
+    for(i = 1; i <= lastDate; i++){
+        if(i === new Date().getDate() &&
+        date.getMonth() === new Date().getMonth() &&
+        date.getFullYear() === new Date().getFullYear()
+        ){
+            days += `<div class="today day" id=${i}>${i}</div>`
+        }else{
+            days += `<div class="day" id=${i}>${i}</div>`
+        }
+    };
+
+    //next month days
+    for(j = 1; j <= nextMonthDays; j++){
+        days += `<div class="next-date day" id=${j}>${j}</div>`
+    }
+    monthDays.innerHTML = days;
+
+    //hide and show arrow icons
+    if(date.getFullYear() <= new Date().getFullYear() &&
+    date.getMonth() <= new Date().getMonth()){
+        prevYear.classList.add('none');
+        prevMonth.classList.add('none');
+
+    } else if (date.getFullYear() <= new Date().getFullYear()){
+        prevYear.classList.add('none')
+    }
+
+    if(date.getFullYear() >= new Date().getFullYear() &&
+    date.getMonth() >= new Date().getMonth()+1) {
+        prevMonth.classList.remove('none');
+
+    } 
+    if (date.getFullYear() > new Date().getFullYear()){
+        prevYear.classList.remove('none');
+        prevMonth.classList.remove('none');
+
+    }
+
+};
+prevMonth.addEventListener('click', () =>{
+    date.setMonth(date.getMonth()-1);
+    renderCalendar();
+});
+nextMonth.addEventListener('click', () => {
+    date.setMonth(date.getMonth()+1);
+    renderCalendar();
+});
+prevYear.addEventListener('click', () =>{
+    date.setFullYear(date.getFullYear()-1);
+    renderCalendar();
+});
+nextYear.addEventListener('click', () => {
+    date.setFullYear(date.getFullYear()+1);
+    renderCalendar();
+});
+
+//listen for selected days
+monthDays.addEventListener('click', e => {
+    if(e.target.classList.contains('day')){
+        const selectedDay = e.target.id;
+        console.log(selectedDay);
+    }
+});
+renderCalendar();
+
