4e41f9244da926751c969e4508149410df2cc928
Joanna Fisher <fisherjoanna@sky.com>
2021-01-08 17:52:16 +0000

    calendar component highlights a range of dates

--- src/scripts/calendar.js
+++ src/scripts/calendar.js
@@ -20,7 +20,10 @@ class Calendar {
         this.prevYear = document.querySelector('.prev-year');
         this.nextYear = document.querySelector('.next-year');
         this.monthDays = document.querySelector('.days');
-        this.dateEnquiry = document.querySelector('.date-enquiry')
+        this.dateEnquiry = document.querySelector('.date-enquiry');
+        this.selectedDateCounter = 0;
+        this.selectedDateId = null;
+        // this.allDays = document.querySelectorAll('.day');
     };
     
 
@@ -58,38 +61,40 @@ class Calendar {
     
         document.querySelector('.month').innerHTML = this.month[this.date.getMonth()];
         document.querySelector('.year').innerHTML = this.date.getFullYear();
-        console.log(this.month[this.date.getMonth()])
-        console.log(this.date.getFullYear())
+        // console.log(this.month[this.date.getMonth()])
+        // console.log(this.date.getFullYear())
     
         document.querySelector('.selected-date').innerHTML = new Date().toDateString();
     
         let days = "";
-        console.log(days, firstDayIndex)
+        let arrayIndex = 0;
+        // console.log(days, firstDayIndex)
 
         //count down to output last month days
         for(let x = firstDayIndex; x > 0; x--){
-            // console.log(x)
-            days += `<div class="prev-date day" id=${prevLastDate - x + 1}>${prevLastDate - x + 1}</div>`
-            console.log(days)
+            arrayIndex++;
+            days += `<div class="prev-date day" id=${arrayIndex}>${prevLastDate - x + 1}</div>`
+            // console.log(days)
         };
     
         //this month days
         for(let i = 1; i <= lastDate; i++){
+            arrayIndex++;
             if(i === new Date().getDate() &&
             this.date.getMonth() === new Date().getMonth() &&
             this.date.getFullYear() === new Date().getFullYear()
             ){
-                days += `<div class="today day" id=${i}>${i}</div>`
+                days += `<div class="today day" id=${arrayIndex}>${i}</div>`
             }else{
-                days += `<div class="day" id=${i}>${i}</div>`
+                days += `<div class="day" id=${arrayIndex}>${i}</div>`
             }
         };
     
         //next month days
         for(let j = 1; j <= nextMonthDays; j++){
-            days += `<div class="next-date day" id=${j}>${j}</div>`
+            arrayIndex++;
+            days += `<div class="next-date day" id=${arrayIndex}>${j}</div>`
         };
-        console.log('test2');
         this.monthDays.innerHTML = days;
     
         //hide and show arrow icons
@@ -113,21 +118,47 @@ class Calendar {
     
         };
         //listen for selected days
+        //add conditional statement to check if id is before today
         this.monthDays.addEventListener('click', e => {
             if(e.target.classList.contains('day')){
-                e.target.classList.add('clicked');
-                const selectedDay = e.target.id;
-                this.selectDays(selectedDay);
+                const dateId = parseInt(e.target.id);
+                this.selectDays(e.target, dateId);
             }
         });
     
     };
-    
-    selectDays(selectedDay) {
-        console.log(selectedDay)
-        this.dateEnquiry.innerHTML = new Date(this.date.getFullYear(), this.date.getMonth(), selectedDay).toDateString();
-    };
 
+
+    
+    selectDays(highlight, dateId) {
+        this.selectedDateCounter ++;
+        if(this.selectedDateCounter <= 2 && (!this.selectedDateId || this.selectedDateId < dateId)){
+            highlight.classList.add('clicked');
+            if(this.selectedDateCounter === 2){
+                // console.log('date range')
+                // console.log(this.selectedDateId, dateId)
+                for(let x = this.selectedDateId +1; x < dateId; x++){
+                    let a = x.toString();
+                    console.log(a)
+                    let b = document.getElementById(a);
+                    b.classList.add('clicked-range');
+                }
+                this.selectedDateId = null;
+            } else {
+                this.selectedDateId = parseInt(dateId);
+            }
+
+        } else {
+            document.querySelectorAll('.day').forEach(item => {
+                item.classList.remove('clicked');
+                item.classList.remove('clicked-range');
+                this.selectedDateCounter = 1;
+                this.selectedDateId = parseInt(dateId);
+                highlight.classList.add('clicked');
+            });
+        this.dateEnquiry.innerHTML = new Date(this.date.getFullYear(), this.date.getMonth(), dateId).toDateString();
+    };
+    }
 };
 
 
